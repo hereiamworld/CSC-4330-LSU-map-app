c45345d64c92a821c9dd1a70cad0e5beaabc7389
Alexandre Knijn <aknijn1@lsu.edu>
2017-04-19 19:31:31 -0500

    add full android studio project file
    (also added pin support)

---
+++ LSU(M)app/app/src/main/java/lsumapp/lsumapp/MapsActivity.java
@@ -0,0 +1,120 @@
+package lsumapp.lsumapp;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/****************************************************************************************/
+/*
+/* FILE NAME: MapsActivity.java
+/*
+/* DESCRIPTION: main program file
+/*
+/*   DATE     	       BY                     DESCRIPTION
+/* ======== 	================	=======================
+/*  4/15/17 	 Kyle Eastin		created the file
+/*  4/19/17	     Alexandre Knijn	added pin creation on mouse long click
+/*
+/*
+/*
+/****************************************************************************************/
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Geocoder geocoder;
+    Marker marker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker at LSU Memorial Tower and move the camera
+        LatLng startup = new LatLng(30.414498,-91.178913);
+        mMap.addMarker(new MarkerOptions().position(startup).title("War Memorial Tower"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startup,16));
+
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            /**
+             * Executed when a long click is done on the map.
+             *
+             * @return void
+             * @param point: latitude and longitude of clicked point
+             * @throws null
+             */
+            @Override
+            public void onMapLongClick(LatLng point) {
+                //save current location
+                LatLng clickedLatLng = point;
+
+                List<Address> addresses = new ArrayList<>();
+                try {
+                    addresses = geocoder.getFromLocation(point.latitude, point.longitude,1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                android.location.Address address = addresses.get(0);
+
+                if (address != null) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < address.getMaxAddressLineIndex(); i++){
+                        sb.append(address.getAddressLine(i) + "\n");
+                    }
+                    Toast.makeText(MapsActivity.this, sb.toString(), Toast.LENGTH_LONG).show();
+                }
+
+                //remove previously placed Marker
+                if (marker != null) {
+                    marker.remove();
+                }
+
+                //place marker where user just clicked
+                marker = mMap.addMarker(new MarkerOptions().position(point).title("Marker")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+
+            }
+        });
+
+    }
+}
